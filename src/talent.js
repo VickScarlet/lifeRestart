a68ec446f2968a531463f886e40be1c56c2e2646
cloxkbird <90033017+cloxkbird@users.noreply.github.com>
2021-09-03 18:51:35 +1000

    Update talent.js

--- src/talent.js
+++ src/talent.js
@@ -60,7 +60,7 @@ class Talent {
                 if(!i && include) return include;
                 const gradeRandom = Math.random();
                 let grade;
-                if(gradeRandom>=0.111) grade = 0;
+                if(gradeRandom>=0.111) grade = 3;
                 else if(gradeRandom>=0.011) grade = 1;
                 else if(gradeRandom>=0.001) grade = 2;
                 else grade = 3;
