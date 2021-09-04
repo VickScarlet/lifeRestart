2c4b276686ae70dfa272fa6ad8b844c386fde1ac
SukkaW <isukkaw@gmail.com>
2021-09-04 12:13:49 +0800

    fix: hideBanners being undefined

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from '../src/app.js';
 window.json = async fileName => await (await fetch(`../data/${fileName}.json`)).json();
 
 // Pssst, I've created a github package - https://github.com/brookesb91/dismissible
-const hideBanners = (e) => {
+export const hideBanners = (e) => {
     document
         .querySelectorAll(".banner.visible")
         .forEach((b) => b.classList.remove("visible"));
