dbac251c767ddd436904d0650ccead865084eb23
SukkaW <isukkaw@gmail.com>
2021-09-04 12:15:30 +0800

    refactor: use fetch api instead of axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import App from '../src/app.js';
 
 
-window.json = async fileName=>(await axios(`../data/${fileName}.json`)).data;
+window.json = async fileName => await (await fetch(`../data/${fileName}.json`)).json();
 
 // Pssst, I've created a github package - https://github.com/brookesb91/dismissible
 const hideBanners = (e) => {
